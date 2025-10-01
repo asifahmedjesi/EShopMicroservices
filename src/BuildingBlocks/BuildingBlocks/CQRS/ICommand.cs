d4adba8b84dceff97248b5d33064c1cfca1963d0
Asif Ahmed Jesi <asifahmedjesi@gmail.com>
2025-10-01 22:09:20 +0600

    Catalog microservice completed

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.CQRS;
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{ }
+
+public interface ICommand : ICommand<Unit>
+{ }
---
+++ src/HomeModule.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API;
+
+public class HomeModule : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/", () => "Hello from Carter!");
+    }
+}
